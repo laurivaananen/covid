c4ff32bc2c6cf76e7bd4dcb06ca65cc07e77551a
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-03-17 00:07:02 +0100

    add covid parser

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as d3 from "d3-dsv";
+import "./style.css";
 
-function App() {
+const App = () => {
+  const [covidData, setCovidData] = useState([] as any);
+
+  const FetchData = async () => {
+    const result = await axios(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    );
+    const parsed = d3.csvParse(result.data);
+    console.log(parsed);
+    parsed.sort((row: any, row2: any) => {
+      console.log(row["3/15/20"]);
+      return Number.parseInt(row2["3/15/20"]) - Number.parseInt(row["3/15/20"]);
+    });
+    setCovidData(parsed);
+  };
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="covid-list">
+        <ul className="covid-country">
+          <li>Country</li>
+          <li>Total</li>
+        </ul>
+        {covidData.map((x: any) => {
+          return (
+            <ul className="covid-country">
+              <li>{x["Country/Region"]}</li>
+              <li>{x["3/15/20"]}</li>
+            </ul>
+          );
+        })}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
