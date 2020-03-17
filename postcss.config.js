2262d0440b0334e98efa6a51ea276a4319591037
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-03-17 22:34:28 +0100

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,15 @@
-const tailwindcss = require("tailwindcss");
-const purgecss = require("@fullhuman/postcss-purgecss");
-const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  // Specify the paths to all of the template files in your project
+  content: ["./src/**/*.js", "./public/index.html"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
 
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.config.js"),
-    autoprefixer(),
-    purgecss({
-      content: ["./src/**/*.html"]
-    })
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import * as d3 from "d3-dsv";
 
 const App = () => {
   const [covidData, setCovidData] = useState("");
-  // const App = () => {
-  //   const [covidData, setCovidData] = useState(([] as unknown) as d3.DSVRowArray<
-  //     string
-  //   >);
 
   const FetchData = async () => {
     const result = await axios(
       "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
     );
-    // const parsed = d3.csvParse(result.data);
-    // parsed.sort((row: any, row2: any) => {
-    //   console.log(row["3/15/20"]);
-    //   return Number.parseInt(row2["3/15/20"]) - Number.parseInt(row["3/15/20"]);
-    // });
     setCovidData(result.data);
   };
 
