7aa0e89cb787dc2fe0d7c84bf7540b05ebd3acca
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-03-17 21:44:55 +0100

    new style

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const autoprefixer = require("autoprefixer");
 
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.js"),
+    tailwindcss("./tailwind.config.js"),
     autoprefixer(),
     purgecss({
       content: ["./src/**/*.html"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect, useLayoutEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as d3 from "d3-dsv";
 
 const App = () => {
-  const [covidData, setCovidData] = useState([] as any);
-  const [status, setStatus] = useState("");
+  const [covidData, setCovidData] = useState("");
+  // const App = () => {
+  //   const [covidData, setCovidData] = useState(([] as unknown) as d3.DSVRowArray<
+  //     string
+  //   >);
 
   const FetchData = async () => {
-    setStatus("Loading..");
     const result = await axios(
       "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
     );
-    const parsed = d3.csvParse(result.data);
-    console.log(parsed);
-    parsed.sort((row: any, row2: any) => {
-      console.log(row["3/15/20"]);
-      return Number.parseInt(row2["3/15/20"]) - Number.parseInt(row["3/15/20"]);
-    });
-    setCovidData(parsed);
-    setStatus("");
+    // const parsed = d3.csvParse(result.data);
+    // parsed.sort((row: any, row2: any) => {
+    //   console.log(row["3/15/20"]);
+    //   return Number.parseInt(row2["3/15/20"]) - Number.parseInt(row["3/15/20"]);
+    // });
+    setCovidData(result.data);
   };
 
   useEffect(() => {
@@ -26,22 +26,21 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className="text-gray-800 text-md leading-5 antialiased">
       <ul>
-        <ul className="text-green-300">
-          <li>Country</li>
-          <li>Total</li>
-        </ul>
-        {status && (
-          <ul>
-            <li>{status}</li>
-          </ul>
-        )}
-        {covidData.map((x: any) => {
+        {covidData.split("\n").map((row: string, index: number) => {
           return (
-            <ul>
-              <li>{x["Country/Region"]}</li>
-              <li>{x["3/15/20"]}</li>
+            <ul className={`flex py-2 ${index % 2 ? "bg-gray-200" : ""}`}>
+              {row
+                .split(",")
+                .filter(
+                  (_columnName: string, index: number, columns: string[]) => {
+                    return index < 2 || index > columns.length - 4;
+                  }
+                )
+                .map((value: string) => {
+                  return <li className="flex-1">{value}</li>;
+                })}
             </ul>
           );
         })}
