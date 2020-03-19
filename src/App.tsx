c83e2411b048bf42823946c23c58e397ef7308b3
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-03-19 22:02:52 +0100

    new api

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import csvParser from "csv-parse/lib/sync";
+
+export interface ILocation {
+  lat: number;
+  lng: number;
+}
+
+export interface ICountrycode {
+  iso2: string;
+  iso3: string;
+}
+
+export interface ICountry {
+  provincestate: string;
+  countryregion: string;
+  lastupdate: Date;
+  location: ILocation;
+  countrycode: ICountrycode;
+  confirmed: number;
+  deaths: number;
+  recovered: number;
+}
+
+export interface IInfectionStateRatioPercentages {
+  deaths: number;
+  confirmed: number;
+  recovered: number;
+}
+
+export interface ICountryStats extends ICountry {
+  infectionStateRatioPercentages: IInfectionStateRatioPercentages;
+}
 
 const App = () => {
-  const [covidDataInfected, setCovidDataInfected] = useState([] as Array<
-    string[]
-  >);
-  const [covidDataDeaths, setCovidDataDeaths] = useState([] as Array<string[]>);
-  const [covidDataRecovered, setCovidDataRecovered] = useState([] as Array<
-    string[]
-  >);
+  const [globalData, setGlobalData] = useState([] as ICountry[]);
 
   const FetchData = async () => {
-    const infectedResponse = await axios(
-      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    const globalData = await axios(
+      "https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest"
     );
-    const infectedData = csvParser(infectedResponse.data);
-
-    const deathsResponse = await axios(
-      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+    globalData.data.sort(
+      (country: ICountry, country2: ICountry) =>
+        country2.confirmed - country.confirmed
     );
-    const deathsData = csvParser(deathsResponse.data);
-
-    const revoredResponse = await axios(
-      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
-    );
-    const recoveredData = csvParser(revoredResponse.data);
-
-    // sortedData.sort((row: string, row2: string) => {
-    //   return (
-    //     Number.parseInt(row2[row2.length - 1]) -
-    //     Number.parseInt(row[row.length - 1])
-    //   );
-    // });
-    setCovidDataDeaths(deathsData);
-    setCovidDataRecovered(recoveredData);
-    setCovidDataInfected(infectedData);
+    setGlobalData(globalData.data);
   };
 
   useEffect(() => {
@@ -46,123 +54,86 @@ const App = () => {
     <div className="text-gray-800 sm:text-base text-sm leading-5 antialiased">
       <ul>
         <li>
-          <ul className={`flex py-4 w-full`}>
-            <li className="font-bold w-3/12 mx-2 sm:mx-4">Region</li>
-            {/* <li className="font-bold">Total Infected</li>
-          <li className="font-bold">Total Deaths</li>
-          <li className="font-bold">Total Recovered</li> */}
-            <li className="font-bold w-6/12 mx-2 sm:mx-4">
-              <span className="text-red-400">Deaths</span>
-              <span className="font-normal text-gray-500 sm:px-4"> / </span>
-              <span className="text-yellow-400">Infections</span>
-              <span className="font-normal text-gray-500 sm:px-4"> / </span>
-              <span className="text-green-400">Recoveries</span>
+          <ul className="flex">
+            <li className="font-bold mx-2 sm:mx-4 sm:w-2/12 w-4/12 text-xl py-4">
+              Region
             </li>
-            <li className="font-bold w-3/12 mx-2 sm:mx-4">
-              Infections Past Week
+            <li className="font-bold mx-2 sm:mx-4 sm:w-10/12 w-8/12 flex">
+              <span className="text-red-600 bg-red-400 text-5xl inline-block text-center flex-1 py-4">
+                ✝
+              </span>
+              <span className="text-yellow-600 bg-yellow-400 text-5xl flex-1 inline-block text-center py-4">
+                ☣️
+              </span>
+              <span className="text-green-600 bg-green-400 text-5xl flex-1 text-center py-4">
+                👍
+              </span>
             </li>
           </ul>
         </li>
-        {covidDataInfected
-          .slice(1)
-          .map((row: string[], index: number) => {
-            const newRow: Array<string | number> = [];
-            if (row[0] === row[1] || !row[0]) {
-              newRow.push(row[1]);
-            } else {
-              const region = `${row[1]}, ${row[0]}`;
-              newRow.push(region);
-            }
-            const totalInfections = Number.parseInt(row[row.length - 1]);
-            newRow.push(totalInfections);
+        {globalData
+          .map((country: ICountry) => {
+            const totalCases =
+              country.confirmed + country.deaths + country.recovered;
 
-            const totalDeaths = Number.parseInt(
-              covidDataDeaths[index + 1][covidDataDeaths[index + 1].length - 1]
-            );
-            // newRow.push(totalDeaths);
+            const infectionStateRatioPercentages: IInfectionStateRatioPercentages = {
+              deaths: (country.deaths / totalCases) * 100,
+              confirmed: (country.confirmed / totalCases) * 100,
+              recovered: (country.recovered / totalCases) * 100
+            };
 
-            const totalRecovered = Number.parseInt(
-              covidDataRecovered[index + 1][
-                covidDataRecovered[index + 1].length - 1
-              ]
-            );
-            // newRow.push(totalRecovered);
-
-            const totalCases = totalInfections + totalDeaths + totalRecovered;
-            const infectionsPercent = (totalInfections / totalCases) * 100;
-            newRow.push(infectionsPercent);
-            const deathsPercent = (totalDeaths / totalCases) * 100;
-            newRow.push(deathsPercent);
-            const recoveredPercent = (totalRecovered / totalCases) * 100;
-            newRow.push(recoveredPercent);
-
-            const threeDaysAgo = Number.parseInt(row[row.length - 8]);
-            const difference = totalInfections - threeDaysAgo;
-            const percentage = (difference / threeDaysAgo) * 100;
-            newRow.push(percentage.toFixed(1));
-
-            return newRow;
+            return Object.assign({}, country, {
+              infectionStateRatioPercentages
+            });
           })
-          .map((row: Array<string | number>, index: number) => {
+          .map((country: ICountryStats, index: number) => {
             return (
-              <li key={row.join("")}>
+              <li>
                 <ul
                   className={`flex py-2 ${
-                    !(index % 2) ? "bg-gray-200" : ""
+                    !(index % 2) ? "bg-gray-100" : ""
                   } w-full`}
                 >
-                  {row.map((value: string | number, index: number, orig) => {
-                    if (index === orig.length - 4) {
-                      return (
-                        <li className="w-6/12 mx-2 sm:mx-4">
-                          <div className="bg-gray-100 h-full flex">
-                            <div
-                              className="bg-red-400 h-full"
-                              style={{ width: `${row[index + 1]}%` }}
-                            ></div>
-                            <div
-                              className="bg-yellow-400 h-full flex"
-                              style={{ width: `${row[index]}%` }}
-                            >
-                              <span className="m-auto text-yellow-600 font-bold">
-                                {row[index - 1]}
-                              </span>
-                            </div>
-                            <div
-                              className="bg-green-400 h-full"
-                              style={{ width: `${row[index + 2]}%` }}
-                            ></div>
-                          </div>
-                        </li>
-                      );
-                    }
-                    if (index === orig.length - 1) {
-                      if (Math.sign(value as number) > 0) {
-                        return (
-                          <li className="w-3/12 flex mx-2">
-                            <span className="text-red-500 font-bold my-auto">
-                              +{value}%
-                            </span>
-                          </li>
-                        );
-                      } else if (Math.sign(value as number) < 0) {
-                        return (
-                          <li className="text-green-400 font-bold w-3/12 mx-2">
-                            {value}%
-                          </li>
-                        );
-                      } else {
-                        return (
-                          <li className="text-yellow-500 font-bold w-3/12 mx-2"></li>
-                        );
-                      }
-                    }
-                    if (index === 0) {
-                      return (
-                        <li className="mx-2 sm:mx-4 w-3/12 mx-2">{value}</li>
-                      );
-                    }
-                  })}
+                  <li className="mx-2 sm:mx-4 sm:w-2/12 w-4/12">
+                    {country.countryregion}
+                  </li>
+                  <li className="mx-2 sm:mx-4 sm:w-10/12 w-8/12">
+                    <div className="bg-gray-200 h-full flex">
+                      <div
+                        className="bg-red-400 h-full flex"
+                        style={{
+                          width: `${country.infectionStateRatioPercentages.deaths}%`
+                        }}
+                      >
+                        <span className="m-auto text-red-600 font-bold overflow-x-hidden">
+                          {country.infectionStateRatioPercentages.deaths > 10 &&
+                            country.deaths}
+                        </span>
+                      </div>
+                      <div
+                        className="bg-yellow-400 h-full flex"
+                        style={{
+                          width: `${country.infectionStateRatioPercentages.confirmed}%`
+                        }}
+                      >
+                        <span className="m-auto text-yellow-600 font-bold overflow-x-hidden">
+                          {country.infectionStateRatioPercentages.confirmed >
+                            10 && country.confirmed}
+                        </span>
+                      </div>
+                      <div
+                        className="bg-green-400 h-full flex"
+                        style={{
+                          width: `${country.infectionStateRatioPercentages.recovered}%`
+                        }}
+                      >
+                        <span className="m-auto text-green-600 font-bold overflow-x-hidden">
+                          {country.infectionStateRatioPercentages.recovered >
+                            10 && country.recovered}
+                        </span>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
               </li>
             );
