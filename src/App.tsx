42e2cd30f9f50e43168b0c6360c531b78650729f
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-03-25 19:36:31 +0100

    new api

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,82 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-export interface ILocation {
-  lat: number;
-  lng: number;
-}
-
-export interface ICountrycode {
-  iso2: string;
-  iso3: string;
-}
+import parse from "csv-parse/lib/sync";
 
 export interface ICountry {
-  provincestate: string;
-  countryregion: string;
-  lastupdate: Date;
-  location: ILocation;
-  countrycode: ICountrycode;
-  timeseries: ITimeseries;
+  region: string;
   isOpen: boolean;
+  total: ICaseStatistics;
+  changePastWeek: ICaseStatistics;
 }
 
-export interface IDailyStatistic {
-  confirmed?: number;
-  deaths?: number;
-  recovered: number;
-}
-
-export interface ITimeseries {
-  [key: string]: IDailyStatistic;
-}
-
-export interface IInfectionStateRatioPercentages {
-  deaths: number;
+export interface ICaseStatistics {
   confirmed: number;
-  recovered: number;
 }
 
-export interface ILatestData {
-  deaths: number;
-  confirmed: number;
-  recovered: number;
-}
+// const countryLatestData = (country: ICountry): IDailyStatistic => {
+//   const latest = Object.values(country.timeseries)[
+//     Object.keys(country.timeseries).length - 1
+//   ];
+//   console.log(latest.recovered);
+//   if (!latest) {
+//     return { confirmed: 0, deaths: 0, recovered: 0 };
+//   }
+//   return latest;
+// };
 
-export interface IPastWeekChanges {
-  confirmed: number;
-  recovered: number;
-  deaths: number;
-}
+// const countryPastWeek = (country: ICountry): IDailyStatistic => {
+//   const latest = Object.values(country.timeseries)[
+//     Object.keys(country.timeseries).length - 8
+//   ];
+//   if (!latest) {
+//     return { confirmed: 0, deaths: 0, recovered: 0 };
+//   }
+//   return latest;
+// };
 
-export interface ICountryStats extends ICountry {
-  infectionStateRatioPercentages: IInfectionStateRatioPercentages;
-  latestData: ILatestData;
-  pastWeekChanges: IPastWeekChanges;
-}
-
-const countryLatestData = (country: ICountry): IDailyStatistic => {
-  const latest = Object.values(country.timeseries)[
-    Object.keys(country.timeseries).length - 1
-  ];
-  console.log(latest.recovered);
-  if (!latest) {
-    return { confirmed: 0, deaths: 0, recovered: 0 };
-  }
-  return latest;
-};
-
-const countryPastWeek = (country: ICountry): IDailyStatistic => {
-  const latest = Object.values(country.timeseries)[
-    Object.keys(country.timeseries).length - 8
-  ];
-  if (!latest) {
-    return { confirmed: 0, deaths: 0, recovered: 0 };
-  }
-  return latest;
+const calculateConfirmedPastWeekChange = (countryRow: string[]) => {
+  const latest = Number.parseInt(countryRow[countryRow.length - 1]);
+  const pastWeek = Number.parseInt(countryRow[countryRow.length - 8]);
+  const confirmedRatePercentagePastWeek =
+    ((latest - pastWeek) / pastWeek) * 100;
+  return confirmedRatePercentagePastWeek;
 };
 
 const App = () => {
   const [globalData, setGlobalData] = useState([] as ICountry[]);
 
   const FetchData = async () => {
-    const globalData = await axios(
-      "https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest?onlyCountries=true"
+    const confirmedTimeSeries = await axios(
+      "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
     );
-    globalData.data.sort(
-      (country: ICountry, country2: ICountry) =>
-        (countryLatestData(country2).confirmed || 0) -
-        (countryLatestData(country).confirmed || 0)
-    );
-
-    setGlobalData(
-      globalData.data.map((country: ICountry, index: number) =>
-        Object.assign({}, country, { isOpen: index === 0 ? true : false })
-      )
-    );
+    const parsedData = parse(confirmedTimeSeries.data);
+    const cleanedData: ICountry[] = parsedData
+      .slice(1)
+      .map((countryRow: string[], index: number) => {
+        const countryData: ICountry = {
+          region: buildRegionName(countryRow[1], countryRow[0]),
+          isOpen: index === 0 ? true : false,
+          total: {
+            confirmed: Number.parseInt(countryRow[countryRow.length - 1])
+          },
+          changePastWeek: {
+            confirmed: calculateConfirmedPastWeekChange(countryRow)
+          }
+        };
+        return countryData;
+      })
+      .sort(
+        (country1: ICountry, country2: ICountry) =>
+          country2.total.confirmed - country1.total.confirmed
+      );
+    console.log(cleanedData);
+    setGlobalData(cleanedData);
   };
 
   const toggleCountry = (country: ICountry) => {
     setGlobalData(
       globalData.map(countryData => {
-        if (
-          buildRegionName(
-            countryData.countryregion,
-            countryData.provincestate
-          ) === buildRegionName(country.countryregion, country.provincestate)
-        ) {
+        if (countryData.region === country.region) {
           return { ...countryData, isOpen: !countryData.isOpen };
         }
         return countryData;
@@ -120,88 +92,35 @@ const App = () => {
     <div className="text-gray-800 sm:text-base text-sm antialiased">
       <ul>
         <li>
-          <ul className="flex">
-            <li className="font-bold mx-1 sm:mx-2 sm:mx-4 sm:w-2/12 w-5/12 text-xl py-4">
+          <div className="flex items-center">
+            <div className="font-bold mx-1 sm:mx-2 sm:mx-4 sm:w-2/12 w-5/12 text-xl text-center">
               Region
-            </li>
-            <li className="font-bold mx-1 sm:mx-2 sm:w-10/12 w-7/12 flex">
-              <span className="text-red-600 bg-red-400 text-5xl inline-block text-center flex-1 py-4">
-                <i className="fas fa-cross"></i>
-              </span>
-              <span className="text-yellow-600 bg-yellow-400 text-5xl flex-1 inline-block text-center py-4">
+            </div>
+            <div className="font-bold mx-1 sm:mx-2 sm:w-10/12 w-7/12 flex">
+              <span className="text-yellow-600 bg-yellow-400 text-5xl flex-1 text-center">
                 <i className="fas fa-biohazard"></i>
+                <span className="text-sm block">Confirmed</span>
               </span>
-              <span className="text-green-600 bg-green-400 text-5xl flex-1 text-center py-4">
-                <i className="fas fa-heart"></i>
-              </span>
-            </li>
-          </ul>
+            </div>
+          </div>
         </li>
-        {globalData
-          .map((country: ICountry) => {
-            const latestDeaths = countryLatestData(country).deaths || 0;
-            const latestRecovered = countryLatestData(country).recovered || 0;
-            const latestConfirmed = countryLatestData(country).confirmed || 0;
-            const totalCases = latestConfirmed + latestDeaths + latestRecovered;
-
-            const pastWeekConfirmed = countryPastWeek(country).confirmed || 0;
-            const confirmedRatePercentagePastWeek =
-              ((latestConfirmed - pastWeekConfirmed) / pastWeekConfirmed) * 100;
-
-            const pastWeekDeaths = countryPastWeek(country).deaths || 0;
-            const deathRatePercentagePastWeek =
-              ((latestDeaths - pastWeekDeaths) / pastWeekDeaths) * 100;
-
-            const pastWeekRecovered = countryPastWeek(country).deaths || 0;
-            const recoveredRatePercentagePastWeek =
-              ((latestRecovered - pastWeekRecovered) / pastWeekRecovered) * 100;
-
-            const infectionStateRatioPercentages: IInfectionStateRatioPercentages = {
-              deaths: (latestDeaths / totalCases) * 100,
-              confirmed: (latestConfirmed / totalCases) * 100,
-              recovered: (latestRecovered / totalCases) * 100
-            };
-
-            const aa: ICountryStats = Object.assign(
-              {},
-              country,
-              {
-                infectionStateRatioPercentages
-              },
-              {
-                latestData: {
-                  deaths: latestDeaths,
-                  confirmed: latestConfirmed,
-                  recovered: latestRecovered
-                }
-              },
-              {
-                pastWeekChanges: {
-                  deaths: deathRatePercentagePastWeek,
-                  recovered: recoveredRatePercentagePastWeek,
-                  confirmed: confirmedRatePercentagePastWeek
-                }
-              }
-            );
-            return aa;
-          })
-          .map((country: ICountryStats, index: number) => {
-            return (
-              <CountryItem
-                key={`${country.countryregion}${country.provincestate}`}
-                country={country}
-                index={index}
-                toggleCountry={toggleCountry}
-              />
-            );
-          })}
+        {globalData.map((country: ICountry, index: number) => {
+          return (
+            <CountryItem
+              key={`${country.region}`}
+              country={country}
+              index={index}
+              toggleCountry={toggleCountry}
+            />
+          );
+        })}
       </ul>
     </div>
   );
 };
 
 interface ICountryItemProps {
-  country: ICountryStats;
+  country: ICountry;
   index: number;
   toggleCountry: (country: ICountry) => void;
 }
@@ -225,51 +144,28 @@ const CountryItem: React.FunctionComponent<ICountryItemProps> = ({
                 : "fa-chevron-circle-right"
             } text-gray-400 sm:mr-2 mr-1`}
           ></i>
-          {buildRegionName(country.countryregion, country.provincestate)}
-          {country.pastWeekChanges.confirmed > 100 && (
+          {country.region}
+          {country.changePastWeek.confirmed > 100 && (
             <i className="fas fa-angle-double-up text-red-300 sm:ml-2 ml-1"></i>
           )}
-          {country.pastWeekChanges.confirmed < 100 &&
-            country.pastWeekChanges.confirmed > 0 && (
+          {country.changePastWeek.confirmed < 100 &&
+            country.changePastWeek.confirmed > 0 && (
               <i className="fas fa-angle-up text-orange-300 sm:ml-2 ml-1"></i>
             )}
-          {country.pastWeekChanges.confirmed < 0 && (
+          {country.changePastWeek.confirmed < 0 && (
             <i className="fas fa-angle-down text-green-300 sm:ml-2 ml-1"></i>
           )}
         </div>
         <div className="px-1 sm:px-4 md:w-10/12 w-7/12">
           <div className="bg-gray-200 h-full flex">
             <div
-              className="bg-red-400 h-full flex"
-              style={{
-                width: `${country.infectionStateRatioPercentages.deaths}%`
-              }}
-            >
-              <span className="m-auto text-red-600 font-bold overflow-x-hidden">
-                {country.infectionStateRatioPercentages.deaths > 10 &&
-                  country.latestData.deaths}
-              </span>
-            </div>
-            <div
               className="bg-yellow-400 h-full flex"
               style={{
-                width: `${country.infectionStateRatioPercentages.confirmed}%`
+                width: `100%`
               }}
             >
               <span className="m-auto text-yellow-600 font-bold overflow-x-hidden">
-                {country.infectionStateRatioPercentages.confirmed > 10 &&
-                  country.latestData.confirmed}
-              </span>
-            </div>
-            <div
-              className="bg-green-400 h-full flex"
-              style={{
-                width: `${country.infectionStateRatioPercentages.recovered}%`
-              }}
-            >
-              <span className="m-auto text-green-600 font-bold overflow-x-hidden">
-                {country.infectionStateRatioPercentages.recovered > 10 &&
-                  country.latestData.recovered}
+                {country.total.confirmed}
               </span>
             </div>
           </div>
@@ -282,17 +178,7 @@ const CountryItem: React.FunctionComponent<ICountryItemProps> = ({
       >
         <div>
           Infection Rate Past Week:{" "}
-          {buildInfectionRatePercentage(country.pastWeekChanges.confirmed)}
-        </div>
-        <div>
-          Death Rate Past Week:{" "}
-          {buildInfectionRatePercentage(country.pastWeekChanges.deaths)}
-        </div>
-        <div>
-          Recovery Rate Past Week:{" "}
-          <span className={`text-green-400 font-bold`}>
-            +{country.pastWeekChanges.recovered.toFixed(2)}%
-          </span>
+          {buildInfectionRatePercentage(country.changePastWeek.confirmed)}
         </div>
       </div>
     </li>
